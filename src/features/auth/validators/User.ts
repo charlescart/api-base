853dbe49fe5b11d2181bdc416b06a677395ded96
Charles Rodriguez <charlesrodriguez19@gmail.com>
2020-06-04 17:49:32 -0300

    Update src/features/auth/validators/User.ts
    Co-authored-by: Ronny Medina <ronnyzte@gmail.com>

--- src/features/auth/validators/User.ts
+++ src/features/auth/validators/User.ts
@@ -14,6 +14,24 @@ export default class User {
 
   // tuve que comentar este, quiero validar que minimo sean 5 digitos la clave
 
-  /* @length(5)
+import { Length, IsEmail, MinLength, IsNotEmpty } from 'class-validator';
+
+export default class UserDto {
+  @IsNotEmpty()
+  @Length(3, 15)
+  name: string | undefined;
+
+  @IsNotEmpty()
+  @Length(3, 15)
+  surname: string | undefined;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string | undefined;
+
+  @IsNotEmpty()
+  @MinLength(5)
+  password: string | undefined;
+}
   readonly password: string | undefined; */
 }
